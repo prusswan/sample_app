56663d4bb8d9b50e3b3036ab1865ddd67eeb1b84
prusswan <prusswan@gmail.com>
2012-01-20 01:33:26 +0800

    Added a Sessions controller and a signin view

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-  
+
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
   end
-  
+
   it "should have a Contact page at '/contact'" do
     get '/contact'
     response.should have_selector('title', :content => "Contact")
@@ -16,15 +16,20 @@ describe "LayoutLinks" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end
-  
+
   it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
-  
+
   it "should have a signup page at '/signup'" do
     get '/signup'
-    response.should have_selector('title', :content => "Sign up")    
+    response.should have_selector('title', :content => "Sign up")
+  end
+
+  it "should have a signin page at '/signin'" do
+    get '/signin'
+    response.should have_selector('title', :content => "Sign in")
   end
 
   it "should have the right links on the layout" do
@@ -40,5 +45,5 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Sign up")
     response.should have_selector('a[href="/"]>img')
   end
-  
+
 end
