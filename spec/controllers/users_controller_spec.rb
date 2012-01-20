2afc60d4ea7b2191e955217945fb5f82648e40d5
prusswan <prusswan@gmail.com>
2012-01-20 14:24:03 +0800

    Protecting edit and update with a before filter

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -195,4 +195,22 @@ describe UsersController do
 
   end
 
+  describe "authentication of edit/update actions" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should deny access to 'edit'" do
+      get :edit, :id => @user
+      response.should redirect_to(signin_path)
+    end
+
+    it "should deny access to 'update'" do
+      put :update, :id => @user, :user => {}
+      response.should redirect_to(signin_path)
+    end
+
+  end
+
 end
