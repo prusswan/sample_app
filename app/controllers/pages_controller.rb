73870d31ec2302c08fd58c1133c9a0e8aeb2f343
prusswan <prusswan@gmail.com>
2012-01-21 19:58:17 +0800

    Microposts interface

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,20 @@
 class PagesController < ApplicationController
+
   def home
     @title = "Home"
+    @micropost = Micropost.new if signed_in?
   end
 
   def contact
     @title = "Contact"
   end
-  
+
   def about
     @title = "About"
   end
-  
+
   def help
     @title = "Help"
-  end  
+  end
+
 end
