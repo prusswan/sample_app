879a00eeebbc1ecee4e899ba71b330f67b908ba7
prusswan <prusswan@gmail.com>
2012-01-22 13:01:14 +0800

    Initial interface for following/followers

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,27 +3,47 @@ require 'spec_helper'
 describe PagesController do
 
   render_views
-  
+
   before(:each) do
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
+
+    describe "when not signed in" do
+      it "returns http success" do
+        get 'home'
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get 'home'
+        response.should have_selector("title",
+                                      :content => "#{@base_title} | Home")
+      end
+
+      it "should have a non-blank body" do
+        get 'home'
+        response.body.should_not =~ /<body>\s*<\/body>/
+      end
     end
-    
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title", 
-                                    :content => "#{@base_title} | Home")
+
+    describe "when signed in" do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector('a', :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector('a', :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      end
     end
-    
-    it "should have a non-blank body" do
-      get 'home'
-      response.body.should_not =~ /<body>\s*<\/body>/
-    end
+
   end
 
   describe "GET 'contact'" do
@@ -33,11 +53,11 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", 
+      response.should have_selector("title",
                                     :content => "#{@base_title} | Contact")
-    end    
+    end
   end
-  
+
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
@@ -45,7 +65,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", 
+      response.should have_selector("title",
                                     :content => "#{@base_title} | About")
     end
   end
@@ -57,8 +77,8 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title", 
+      response.should have_selector("title",
                                     :content => "#{@base_title} | Help")
     end
-  end 
+  end
 end
