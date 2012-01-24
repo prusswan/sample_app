1b13b931c9c431d5a7a6809a67a4c33d5ab131cb
prusswan <prusswan@gmail.com>
2012-01-25 01:36:52 +0800

    added pluralize tests for sidebar micropost counts (ex 11.2) refactored homepage into partials (ex 11.5) added test to check that current user would not see delete links for microposts created by other users (ex 11.6) added wrap helper method for microposts.content (ex 11.8)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -31,8 +31,8 @@ describe PagesController do
     describe "when signed in" do
       before(:each) do
         @user = test_sign_in(Factory(:user))
-        other_user = Factory(:user, :email => Factory.next(:email))
-        other_user.follow!(@user)
+        @other_user = Factory(:user, :email => Factory.next(:email))
+        @other_user.follow!(@user)
       end
 
       it "should have the right follower/following counts" do
@@ -41,6 +41,23 @@ describe PagesController do
                                            :content => "0 following")
         response.should have_selector('a', :href => followers_user_path(@user),
                                            :content => "1 follower")
+      end
+
+      it "should not have delete links to microposts of other users" do
+        Factory(:micropost, :user => @other_user)
+        @user.follow!(@other_user)
+        get :home
+        response.should_not have_selector('a', :href => user_path(@other_user),
+                                               :content => "delete")
+      end
+
+      it "should show the correct micropost count" do
+        Factory(:micropost, :user => @user)
+        get :home
+        response.should have_selector('span.microposts', :content => "1 micropost")
+        Factory(:micropost, :user => @user)
+        get :home
+        response.should have_selector('span.microposts', :content => "2 microposts")
       end
     end
 
